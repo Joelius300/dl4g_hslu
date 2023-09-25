aff842e178ea682df1f9f11289815f737a411a67
Joel L. <joelius300@protonmail.com>
2023-09-25 13:53:30 +0200

    Implement rules using Graf heuristics
    Current scores with 10k games: 96 to 60 against random play

--- jass_bot/agents/rule_based.py
+++ jass_bot/agents/rule_based.py
@@ -1,16 +1,16 @@
 import logging
+from functools import cache
+
 import numpy as np
+from numpy.ma import MaskedArray
+
 from jass.agents.agent import Agent
-from jass.game.const import color_masks, card_strings
+from jass.game.const import *
 from jass.game.game_observation import GameObservation
 from jass.game.rule_schieber import RuleSchieber
 
 
 class RuleBasedAgent(Agent):
-    """
-    Rule based agent.
-    """
-
     def __init__(self):
         # log actions
         self._logger = logging.getLogger(__name__)
@@ -19,7 +19,46 @@ class RuleBasedAgent(Agent):
         # init random number generator
         self._rng = np.random.default_rng()
 
+    # heuristic values from Daniel Graf from 2009: "Jassen auf Basis der Spieltheorie"
+    # score if the color is trump
+    trump_scores = [15, 10, 7, 25, 6, 19, 5, 5, 5]
+    # score if the color is not trump
+    no_trump_scores = [9, 7, 5, 2, 1, 0, 0, 0, 0]
+    # score if obenabe is selected (all colors)
+    obenabe_scores = [14, 10, 8, 7, 5, 0, 5, 0, 0]
+    # score if uneufe is selected (all colors)
+    uneufe_scores = [0, 2, 1, 1, 5, 5, 7, 9, 11]
+    # score threshold to exceed, otherwise push
+    push_threshold = 68
+
+    @cache
+    def _get_graf_scores(self, trump: int):
+        if trump == OBE_ABE:
+            scores = self.obenabe_scores * 4
+        elif trump == UNE_UFE:
+            scores = self.uneufe_scores * 4
+        else:
+            scores = (
+                self.no_trump_scores * trump
+                + self.trump_scores
+                + self.no_trump_scores * (3 - trump)
+            )
+
+        return np.array(scores)
+
     def action_trump(self, obs: GameObservation) -> int:
+        def points_for_trump(trump: int):
+            return np.sum(self._get_graf_scores(trump) * obs.hand)
+
+        trump_scores = [points_for_trump(i) for i in range(MAX_TRUMP + 1)]
+        best_trump = np.argmax(trump_scores)
+
+        if trump_scores[best_trump] < self.push_threshold and obs.declared_trump < 0:
+            return PUSH
+
+        return best_trump
+
+    def _select_trump_by_number_of_cards(self, obs: GameObservation):
         trump = 0
         max_number_in_color = 0
         for c in range(4):
@@ -30,9 +69,40 @@ class RuleBasedAgent(Agent):
         return trump
 
     def action_play_card(self, obs: GameObservation) -> int:
+        # card = self._get_random_card(obs)
+        card = self._get_highest_winning_or_lowest_loosing_card(obs)
+        self._logger.debug("Played card: {}".format(card_strings[card]))
+        return card
+
+    def _get_random_card(self, obs: GameObservation) -> int:
         # cards are one hot encoded
         valid_cards = self._rule.get_valid_cards_from_obs(obs)
         # convert to list and draw a value
-        card = self._rng.choice(np.flatnonzero(valid_cards))
-        self._logger.debug('Played card: {}'.format(card_strings[card]))
-        return card
+        return self._rng.choice(np.flatnonzero(valid_cards))
+
+    def _get_highest_winning_or_lowest_loosing_card(self, obs: GameObservation) -> int:
+        valid_cards = self._rule.get_valid_cards_from_obs(obs)
+        valid_card_ids = np.flatnonzero(valid_cards)
+        cards_masked = MaskedArray(valid_cards, valid_cards == 0)
+        graf_scores = cards_masked * self._get_graf_scores(obs.trump)
+
+        highest_winning_card = None
+        highest_winning_card_score = -1
+        for c in valid_card_ids:
+            if self._will_beat_trick_with_card(c, obs):
+                if graf_scores[c] > highest_winning_card_score:
+                    highest_winning_card_score = graf_scores[c]
+                    highest_winning_card = c
+
+        return highest_winning_card if highest_winning_card is not None else graf_scores.argmin()
+
+    def _will_beat_trick_with_card(self, card: int, obs: GameObservation):
+        """
+        Returns whether the current player will beat the current trick with if they play the provided card.
+        :param card: The card to play
+        :param obs: Current GameObservation
+        :return: True if the player will beat the trick with this card (as far as we know, later players might play higher)
+        """
+        tick = obs.current_trick.copy()
+        tick[obs.nr_cards_in_trick] = card
+        return self._rule.calc_winner(tick, obs.trick_first_player[obs.nr_tricks], obs.trump) == obs.player
--- jass_bot/main.py
+++ jass_bot/main.py
@@ -1,16 +1,18 @@
 import logging
+import sys
+
+from agents.rule_based import RuleBasedAgent
 from jass.arena.arena import Arena
 from jass.agents.agent_random_schieber import AgentRandomSchieber
+from jass.game.rule_schieber import RuleSchieber
+from jass.game.game_util import *
+from jass.game.const import *
 
-from jass_bot.agents.rule_based import RuleBasedAgent
 
-if __name__ == "__main__":
+def tournament_ABAB(agent_type: type, opponent_type: type, rounds=1000):
     logging.basicConfig(level=logging.WARNING)
 
-    agent_type = RuleBasedAgent
-    opponent_type = AgentRandomSchieber
-
-    arena = Arena(nr_games_to_play=1000, save_filename='arena_games')
+    arena = Arena(nr_games_to_play=rounds, save_filename='arena_games')
     arena.set_players(
         agent_type(),
         opponent_type(),
@@ -20,5 +22,35 @@ if __name__ == "__main__":
 
     print(f'Playing {arena.nr_games_to_play} games')
     arena.play_all_games()
-    print(f'Average Points Team 0: {arena.points_team_0.mean():.2f})')
-    print(f'Average Points Team 1: {arena.points_team_1.mean():.2f})')
+    print(f'Average Points Team 0 (ours): {arena.points_team_0.mean():.2f})')
+    print(f'Average Points Team 1 (base): {arena.points_team_1.mean():.2f})')
+
+
+def test_case_valid_card():
+    schieber, trump, move_nr = RuleSchieber(), D, 3
+    trick = np.array([C6, DJ, D10])
+    # highest trump in this trick is DJ
+    # lowest trump in this trick is D10
+    # trumps lower than D10 = D8, D7, D6
+    # trumps higher than DJ = None
+    # indices are in order of DA, DK, DQ, DJ, D10, D9, D8, D7, D6
+    print([DA, DK, DQ, DJ, D10, D9, D8, D7, D6])
+    # therefore D10 > DJ meaning that in get_valid_cards the check lowest_trump_played < current_trick[2]
+    # will think that D10 is higher than DJ and update lowest_trump_played leading to the invalid response
+    # that only trumps below D10 are invalid even though all trumps should be invalid because DJ is the max.
+    hand = get_cards_encoded([DA, D9, D6, S10])
+    # player has something else than trump in hand and is therefore not allowed to play any lower trumps
+    valid_cards = schieber.get_valid_cards(hand, trick, move_nr, trump)
+    print(valid_cards)
+    print(convert_one_hot_encoded_cards_to_str_encoded_list(valid_cards))
+    assert valid_cards[DA] == 0  # lower than DJ, cannot play
+    assert valid_cards[D9] == 0  # lower than DJ, cannot play
+    assert valid_cards[D7] == 0  # lower than DJ, cannot play
+    assert valid_cards[S10] == 1 # not trump, can play
+
+
+if __name__ == "__main__":
+    tournament_ABAB(RuleBasedAgent, AgentRandomSchieber)
+    # test_case_valid_card()
+    # print(count_colors(get_cards_encoded([DA, DQ, D6, S10, S7, C9])))
+
