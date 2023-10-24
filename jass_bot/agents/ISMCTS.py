9899a840400a0d4ca005e89a8287caaf7e23a21d
Joel L. <joelius300@protonmail.com>
2023-10-24 07:59:40 +0200

    Simply change some State to Observation

--- jass_bot/agents/ISMCTS.py
+++ jass_bot/agents/ISMCTS.py
@@ -4,7 +4,7 @@ import logging
 import math
 import time
 import random
-from typing import Callable, Optional
+from typing import Callable, Optional, Self, Union
 
 from heuristics import graf
 from jass.agents.agent import Agent
@@ -18,12 +18,12 @@ from jass.game.rule_schieber import RuleSchieber
 Payoffs = np.ndarray[2, np.dtype[np.float64]]
 
 
-def points_div_by_max(state: GameState):
+def points_div_by_max(state: GameObservation | GameState):
     # all points/payoffs between 0 and 1
     return state.points / np.max(state.points)
 
 
-def point_div_by_norm(state: GameState):
+def point_div_by_norm(state: GameObservation | GameState):
     # sum of all payoffs = 1
     return state.points / np.linalg.norm(state.points)
 
@@ -37,42 +37,40 @@ def UCB1(node: ISMCTS.Node, total_n: int, player: int, c=1.0) -> float:
 
 
 class ISMCTS(Agent):
-    # @dataclass
     class Node:
-        N: int
-        """Number of simulations (random walks) started from this node."""
-        W: Payoffs
-        """Accumulated payoff vectors (one component for each team)."""
-
-        last_played_card: int
-        """Card played to get to this state"""
-        state: GameState
-        """Current game state"""
-        parent: Optional[ISMCTS.Node]
-        """Parent node"""
-        children: Optional[list[ISMCTS.Node]]
-        """Game states that are possible to reach from here by playing one of the valid actions."""
-
-        _remaining_cards: Optional[list[int]]
+        """
+        Search tree node. All the nodes in the tree are from the view of the root node player.
+        """
 
         def __init__(
             self,
-            state: GameState,
+            known_state: GameObservation,
             last_played_card: int,
+            root_player: int,
             parent: Optional[ISMCTS.Node],
         ):
-            self.state = state
-            self.last_played_card = last_played_card
             self.N = 0
+            """Number of simulations (random walks) started from this node."""
             self.W = np.zeros(2)
+            """Accumulated payoff vectors (one component for each team)."""
+
+            self.last_played_card = last_played_card
+            """The last card that was played to get to this state."""
+            self.known_state = known_state
+            """The known state of this node from the view of the root player."""
+            self.root_player = root_player
+
             self.parent = parent
-            self.children = None
-            self._remaining_cards = None
+            self.children: Optional[list[Self]] = None
+            """Nodes that are possible to reach from here by playing one of the valid actions."""
+
+            self._remaining_cards: Optional[List[int]] = None
+            """The cards that can be played from this state on."""
 
         @property
         def is_terminal(self):
             """Is this node at the end of a game (no more valid moves)."""
-            return self.state.nr_tricks == 9
+            return self.known_state.nr_tricks == 9
 
         @property
         def fully_expanded(self):
@@ -97,7 +95,7 @@ class ISMCTS(Agent):
             if self._remaining_cards is None:
                 self._remaining_cards = (
                     convert_one_hot_encoded_cards_to_int_encoded_list(
-                        rule.get_valid_cards_from_state(self.state)
+                        rule.get_valid_cards_from_obs(self.known_state)
                     )
                 )
 
@@ -108,7 +106,7 @@ class ISMCTS(Agent):
         timebudget: float,
         rule: GameRule = None,
         tree_policy: Optional[Callable[[Node, int, int], Node]] = None,
-        rollout: Optional[Callable[[Node], Payoffs]] = None,
+        rollout: Optional[Callable[[Node, GameState], Payoffs]] = None,
         get_payoffs: Optional[Callable[[GameState], Payoffs]] = None,
         ucb1_c_param: Optional[float] = math.sqrt(2),
     ):
@@ -134,8 +132,10 @@ class ISMCTS(Agent):
             get_payoffs if get_payoffs else default_payoff_func
         )
 
-    def _get_payoffs_meta(self, get_payoffs: Callable[[GameState], Payoffs]):
-        def internal_get_payoffs(state: GameState):
+    def _get_payoffs_meta(
+        self, get_payoffs: Callable[[Union[GameObservation, GameState]], Payoffs]
+    ):
+        def internal_get_payoffs(state: GameObservation | GameState):
             assert state.nr_tricks == 9, "Tried to get payoffs from non-terminal game"
             return get_payoffs(state)
 
@@ -144,12 +144,12 @@ class ISMCTS(Agent):
     def UCB1_selection(self, node: Node, total_n: int, player: int, c=1.0) -> Node:
         return max(node.children, key=lambda n: UCB1(n, total_n, player, c))
 
-    def _random_walk(self, node: Node) -> Payoffs:
+    def _random_walk(self, node: Node, sampled_state: GameState) -> Payoffs:
         if node.is_terminal:
-            return self._get_payoffs(node.state)
+            return self._get_payoffs(node.known_state)
 
         sim = GameSim(self._rule)
-        sim.init_from_state(node.state)
+        sim.init_from_state(sampled_state)
 
         while not sim.is_done():
             card = np.random.choice(
@@ -163,9 +163,9 @@ class ISMCTS(Agent):
         return graf.graf_trump_selection(observation)
 
     def action_play_card(self, observation: GameObservation) -> int:
-        return self.start_mcts_from_state(state, self.timebudget)
+        return self.start_mcts_from_obs(observation, self.timebudget)
 
-    def start_mcts_from_state(self, state: GameState, timebudget: float):
+    def start_mcts_from_obs(self, state: GameObservation, timebudget: float):
         self._root = self.Node(state, -1, None)
         # in theory, you could try to determine the last played card and parent, but it's irrelevant
         return self.start_mcts(self._root, timebudget)
@@ -174,18 +174,20 @@ class ISMCTS(Agent):
         """Does MCTS during a certain time_budget (in seconds) from node and returns the best card to play."""
         time_end = time.time() + time_budget
 
-        i = 0
         while time.time() < time_end:
-            i += 1
-            self.mcts(node, i)
+            self.mcts(node)
 
-        self._logger.debug("Explored %i nodes in %.3f seconds", i, time_budget)
         return max(node.children, key=lambda n: n.N).last_played_card
 
     def _selection(self, node: Node, total_plays: int) -> Node:
+        # TODO
+        # Total play now has to be the number of total plays the parent node has
+        # had _when_ this node here has been available to be picked.
         next_node = node
         while not next_node.is_leaf and next_node.fully_expanded:
-            next_node = self._tree_policy(next_node, total_plays, node.state.player)
+            next_node = self._tree_policy(
+                next_node, total_plays, node.known_state.player
+            )
 
         assert (
             next_node.is_leaf or not next_node.fully_expanded
@@ -193,7 +195,7 @@ class ISMCTS(Agent):
 
         return next_node
 
-    def _expansion(self, node: Node) -> Node:
+    def _expansion(self, node: Node, sampled_state: GameState) -> Node:
         assert not node.fully_expanded, "Fully expanded node in _expansion"
 
         # if selected node is terminal, take the payoff directly (rollout will end immediately)
@@ -213,10 +215,18 @@ class ISMCTS(Agent):
             i = random.randint(0, len(remaining_cards) - 1)
 
         sampled_card = remaining_cards.pop(i)
+
         game_sim = GameSim(self._rule)
-        game_sim.init_from_state(node.state)
+        game_sim.init_from_state(sampled_state)
         game_sim.action_play_card(sampled_card)
-        next_node = ISMCTS.Node(game_sim.state, sampled_card, node)
+
+        next_node = ISMCTS.Node(
+            observation_from_state(
+                game_sim.state,
+            ),
+            sampled_card,
+            node,
+        )
         if node.children is None:
             node.children = []
         node.children.append(next_node)
@@ -230,9 +240,10 @@ class ISMCTS(Agent):
             parent.W += payoffs
             parent = parent.parent
 
-    def mcts(self, node: Node, total_plays: int):
+    def mcts(self, node: Node):
         assert not node.is_terminal, "Started mcts from terminal node"
+        sampled_state = self.sample_state(node)
         next_node = self._selection(node, total_plays)
-        next_node = self._expansion(next_node)
-        payoffs = self._rollout(next_node)
+        next_node = self._expansion(next_node, sampled_state)
+        payoffs = self._rollout(next_node, sampled_state)
         self._backpropagation(next_node, payoffs)
