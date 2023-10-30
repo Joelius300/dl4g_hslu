5dde806ba32f5ba779b49f0c8ec9f50134b0282b
Joel L. <joelius300@protonmail.com>
2023-10-30 16:26:50 +0100

    Play ourselves locally
    "error in action_play_card" does not give me any information..

--- jass_bot/examples/arena/arena_play_network.py
+++ jass_bot/examples/arena/arena_play_network.py
@@ -17,8 +17,8 @@ def main():
     # setup the arena
     arena = Arena(nr_games_to_play=1)
     player = AgentRandomSchieber()
-    # my_player = AgentByNetwork('http://localhost:5000/random')
-    my_player = AgentByNetwork('https://lg3bsb3a96.execute-api.eu-central-1.amazonaws.com/dev/random')
+    my_player = AgentByNetwork('http://localhost:8888/ISMCTS')
+    # my_player = AgentByNetwork('https://lg3bsb3a96.execute-api.eu-central-1.amazonaws.com/dev/random')
 
     arena.set_players(my_player, player, my_player, player)
     print('Playing {} games'.format(arena.nr_games_to_play))
