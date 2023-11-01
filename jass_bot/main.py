44a186d3de28af1ac6b2a7d2e8d50fdffcc23d03
Joel L. <joelius300@protonmail.com>
2023-11-01 13:07:08 +0100

    Cleanup main, try with network

--- jass_bot/main.py
+++ jass_bot/main.py
@@ -3,6 +3,7 @@ import logging
 import sys
 from typing import Callable
 
+from jass.agents.agent_by_network import AgentByNetwork
 from jass_bot.agents.CompositeAgent import CompositeAgent
 from jass_bot.heuristics import graf
 from jass_bot.agents.CheatingMCTS import CheatingMCTS
@@ -23,30 +24,32 @@ from jass_bot.strategies.card_strategy import CardStrategy
 from jass_bot.strategies.trump_strategy import TrumpStrategy
 from jass_bot.tournament import tournament_ABAB, round_robin
 
+
+def compare_trump_strategies(timebudget=0.05, n_games=100):
+    random_agent = AgentRandomSchieber()
+    return round_robin(
+        {
+            "ISMCTS w/ Graf": lambda: CompositeAgent(
+                TrumpStrategy.from_function(graf.graf_trump_selection),
+                CardStrategy.from_agent(ISMCTS(timebudget)),
+            ),
+            "ISMCTS w/ Random": lambda: CompositeAgent(
+                TrumpStrategy.from_agent(random_agent),
+                CardStrategy.from_agent(ISMCTS(timebudget)),
+            ),
+            "Random w/ Graf": lambda: CompositeAgent(
+                TrumpStrategy.from_function(graf.graf_trump_selection),
+                CardStrategy.from_agent(random_agent),
+            ),
+            "Random w/ Random": AgentRandomSchieber,
+        },
+        n_games=n_games,
+    )
+
+
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO)
 
-    # tournament_ABAB(RuleBasedAgent, AgentRandomSchieber)
-    # tree_container = GameTreeContainer()
-    # tournament_ABAB(lambda: MiniMaxAgent(tree_container, depth=1), AgentCheatingRandomSchieber, n_games=200)
-    # tournament_ABAB(lambda: AlphaBetaAgent(tree_container, depth=2), AgentCheatingRandomSchieber, n_games=50)
-    # tournament_ABAB(lambda: AlphaBetaAgent(tree_container, depth=3), lambda: MiniMaxAgent(tree_container, depth=1), n_games=30)
-    # test_case_valid_card()
-    # print(count_colors(get_cards_encoded([DA, DQ, D6, S10, S7, C9])))
-    # tournament_ABAB(lambda: CheatingMCTS(timebudget=0.1), AgentCheatingRandomSchieber, n_games=100)
-
-    # tournament_ABAB(lambda: ISMCTS(timebudget=.03), AgentRandomSchieber, n_games=100)
-    # round_robin({
-    #     "ISMCTS (tb=.01)": lambda: ISMCTS(timebudget=.01),
-    #     "ISMCTS (tb=.1)": lambda: ISMCTS(timebudget=.1),
-    #     "Random": AgentRandomSchieber
-    # }, n_games=50)
-
-    timebudget = .05
-    random_agent = AgentRandomSchieber()
-    round_robin({
-        "ISMCTS w/ Graf": lambda: CompositeAgent(TrumpStrategy.from_function(graf.graf_trump_selection), CardStrategy.from_agent(ISMCTS(timebudget))),
-        "ISMCTS w/ Random": lambda: CompositeAgent(TrumpStrategy.from_agent(random_agent), CardStrategy.from_agent(ISMCTS(timebudget))),
-        "Random w/ Graf": lambda: CompositeAgent(TrumpStrategy.from_function(graf.graf_trump_selection), CardStrategy.from_agent(random_agent)),
-        "Random w/ Random": AgentRandomSchieber,
-    }, n_games=500)
+    tournament_ABAB(
+        AgentByNetwork("http://localhost:8888/ISMCTS"), AgentRandomSchieber, n_games=5
+    )
