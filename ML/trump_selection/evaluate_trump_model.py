db177cf1e011defa4d5a4424dc2dfc6ab499af73
Joel L. <joelius300@protonmail.com>
2023-11-17 16:15:35 +0100

    Add trump evaluation script, not sure if it'll work

--- ML/trump_selection/evaluate_trump_model.py
+++ ML/trump_selection/evaluate_trump_model.py
@@ -1,3 +1,38 @@
 # TODO load the trained model and run tournaments against the GRAF baseline with ISMCTS.
 # Multi-threaded tournaments would be pretty nice.
 # The result (score) of this should also be a metric stored in DVC.
+import dvc.api
+import dvclive.live
+
+from agent_definitions import AgentDefinition, TrumpDefs, CardDefs
+from tournament import tournament_multiple_sets
+
+STAGE_NAME = "evaluate_trump_model"
+
+if __name__ == "__main__":
+    params = dvc.api.params_show(STAGE_NAME)[STAGE_NAME]
+    time_budget = params["time_budget"]
+    n_sets = params["n_sets"]
+    checkpoint_path = params["checkpoint_path"]
+    point_threshold = params.get("point_threshold", 1000)
+    num_workers = params.get("num_workers", None)
+    ismcts = CardDefs.ISMCTS(time_budget)
+    base = AgentDefinition(TrumpDefs.graf(), ismcts, True)
+
+    ours = AgentDefinition(TrumpDefs.model(checkpoint_path), ismcts, True)
+    (
+        wins_ours,
+        wins_base,
+        means_ours,
+        means_base,
+        avg_games_played,
+        total_games,
+    ) = tournament_multiple_sets(ours, base, n_sets, point_threshold, num_workers)
+
+    # this should log to the current experiment if there is one but not if there isn't. Not sure if this is correct.
+    live = dvclive.Live(resume=True, save_dvc_exp=False)
+    live.log_metric("win_rate", wins_ours / (wins_ours + wins_base))
+    live.log_metric("mean_points", means_ours)
+    live.log_metric("mean_points_graf", means_base)
+    live.log_metric("avg_set_length", avg_games_played)
+    live.log_metric("total_games", total_games)
