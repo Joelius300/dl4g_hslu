f53d14ee79653e5eeee9dda0f2373028fb2dac58
Joel L. <joelius300@protonmail.com>
2023-12-08 17:34:45 +0100

    Take second best option if PUSH is invalid

--- jass_bot/ML/trump_selection/model_trump_strategy.py
+++ jass_bot/ML/trump_selection/model_trump_strategy.py
@@ -11,7 +11,9 @@ from jass_bot.strategies.trump_strategy import TrumpStrategy
 class ModelTrumpStrategy(TrumpStrategy):
     def __init__(self, checkpoint_path: str):
         device = "cuda" if torch.cuda.is_available() else "cpu"
-        self.model: pl.LightningModule = TrumpSelection.load_from_checkpoint(checkpoint_path, device)
+        self.model: pl.LightningModule = TrumpSelection.load_from_checkpoint(
+            checkpoint_path, device
+        )
         self.model.eval()
 
     def obs_to_tensor(self, obs: GameObservation) -> torch.Tensor:
@@ -22,17 +24,23 @@ class ModelTrumpStrategy(TrumpStrategy):
 
         return torch.tensor(cat, dtype=torch.float32)
 
-    def y_to_trump(self, y: torch.Tensor):
+    def y_to_trump(self, y: torch.Tensor, obs: GameObservation):
         y = y.softmax(dim=-1)
         y = y.detach().cpu().numpy()
 
-        trump = np.argmax(y)
+        sorted_idx = np.argsort(y)
+        best = sorted_idx[-1]
+        best = best if best != PUSH_ALT else PUSH
 
-        return trump if trump != PUSH_ALT else PUSH
+        if best == PUSH and obs.forehand != -1:
+            second_best = sorted_idx[-2]
+            return second_best
+
+        return best
 
     def action_trump(self, obs: GameObservation) -> int:
         with torch.no_grad():
             x = self.obs_to_tensor(obs).to(self.model.device)
             y = self.model(x)
 
-            return self.y_to_trump(y)
+            return self.y_to_trump(y, obs)
