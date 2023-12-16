ff3501b75db2911d63d7846890159142fd0d4f18
Joel L. <joelius300@protonmail.com>
2023-12-16 17:48:58 +0100

    Setup bot service for multiple ISMCTS versions without rebuild

--- jass_bot/bot_service.py
+++ jass_bot/bot_service.py
@@ -2,11 +2,9 @@ import logging
 import math
 import os
 
-from agents.MultiProcessingISMCTS import MultiProcessingISMCTS
-from jass_bot.agents.MultiPlayerAgentContainer import MultiPlayerAgentContainer
+from jass_bot.agents.MultiProcessingISMCTS import MultiProcessingISMCTS
 from jass.agents.agent_random_schieber import AgentRandomSchieber
 from jass.service.player_service_app import PlayerServiceApp
-from jass_bot.agents.ISMCTS import ISMCTS
 
 DEFAULT_TIME_BUDGET = 5
 DEFAULT_NUM_WORKERS = -1
@@ -17,12 +15,10 @@ FLASK_PACKAGE_NAME = "jass_bot"
 
 
 def create_app():
+    logging_level = os.environ.get("LOGGING_LEVEL", DEFAULT_LOGGING_LEVEL)
     time_budget = float(os.environ.get("TIME_BUDGET", DEFAULT_TIME_BUDGET))
     num_workers = int(os.environ.get("NUM_WORKERS", DEFAULT_NUM_WORKERS))
     c_param = float(os.environ.get("C_PARAM", DEFAULT_C_PARAM))
-    logging_level = os.environ.get("LOGGING_LEVEL", DEFAULT_LOGGING_LEVEL)
-
-    print(f"Initialized app with logging level {logging_level} and time budget {time_budget}.")
 
     logging.basicConfig(level=logging_level)
 
@@ -33,15 +29,19 @@ def create_app():
     # and the Jass Server will then use a fallback player (random) to play for me.
     # Also, can ignore_same_player_safety as the implementation is now state-free and tested
     # to ensure there aren't any cross-player issues. This removes the need for MultiPlayerAgentContainer.
-    app.add_player(
-        "ISMCTS",
-        MultiProcessingISMCTS(
-            time_budget,
-            ucb1_c_param=c_param,
-            num_workers=num_workers,
+    players = os.environ["PLAYERS"].split(",")
+    for player_name in players:
+        player = MultiProcessingISMCTS(
+            float(os.environ.get("TIME_BUDGET_" + player_name, time_budget)),
+            num_workers=int(os.environ.get("NUM_WORKERS_" + player_name, num_workers)),
+            ucb1_c_param=float(os.environ.get("C_PARAM_" + player_name, c_param)),
             ignore_same_player_safety=True,
-        ),
-    )
+        )
+        app.add_player(player_name, player)
+        print(
+            f"Player '{player_name}': time budget {player.time_budget}, num workers {player.num_workers}, "
+            f"c param {player.ucb1_c_param}"
+        )
 
     return app
 
